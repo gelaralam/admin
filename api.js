4fc9bbad3749819d5194a0793d4d5e45dee6bfdb
NeDi Sopian <nedisopian@gmail.com>
2026-02-09 20:48:04 +0700

    feat: dynamically set API base URL to localhost for development environments.

--- api.js
+++ api.js
@@ -1,4 +1,6 @@
-const API_BASE_URL = "https://data.gelaralam.id";
+const API_BASE_URL = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
+    ? "http://localhost:3000"
+    : "https://data.gelaralam.id";
 
 export const api = {
     getToken() {
